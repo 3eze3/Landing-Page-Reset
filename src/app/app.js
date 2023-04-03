4276297cb14fb5653934f68c1c21620681a9cf91
Ezequiel Cordova <ezekics33@gmail.com>
2023-04-03 17:39:07 -0500

    Add the function left to right

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,9 @@
-console.log("Hola mundo");
+// ----------- Varibles -----------
+const $categories = document.querySelector(".categories");
+// --- Funcionalidad Categoria ---
+leftToRigth($categories);
+function leftToRigth(element) {
+  window.addEventListener("scroll", () => {
+    element.classList.toggle("categories__show", window.scrollY > 175);
+  });
+}
