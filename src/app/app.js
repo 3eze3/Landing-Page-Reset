05ca044a9bf2181d287d8532997e22f2572bc6cb
Ezequiel Cordova <ezekics33@gmail.com>
2023-04-03 19:23:35 -0500

    Add the Feature  to button of the menu

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,25 @@
 // ----------- Varibles -----------
 const $categories = document.querySelector(".categories");
-// --- Funcionalidad Categoria ---
-leftToRigth($categories);
-function leftToRigth(element) {
+const $numRecipes = document.querySelector(".categories__count");
+const $allCategories = document.querySelectorAll(".categories__item");
+const $menuAddRecipes = document.querySelector(".recipes__menu");
+const $buttonAddRecipes = document.querySelector(".addRecipes__display");
+const $menuAddName = document.querySelector(".addRecipes__recipe");
+const $containerAdd = document.querySelector(".addRecipes__flag");
+// --- Funcionalidad Categoria ---/
+leftToRight($categories);
+function leftToRight(element) {
   window.addEventListener("scroll", () => {
-    element.classList.toggle("categories__show", window.scrollY > 175);
+    element.classList.toggle("categories__show", window.scrollY > 185);
   });
 }
+// --- Funcionalidad ocultar/mostrar menu ---/
+const invert = () => {
+  $buttonAddRecipes.addEventListener("click", () => {
+    $menuAddRecipes.classList.toggle("addRecipes__close");
+    $menuAddName.classList.toggle("addRecipes__recipe--none");
+    $containerAdd.classList.toggle("addRecipes__flag--active");
+  });
+};
+invert();
+// --- Funcionalidad agregar ingredientes ---/
