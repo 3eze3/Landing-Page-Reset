6b612b1b2e757915a72cf4b9986d57e246070679
Ezequiel Cordova <ezekics33@gmail.com>
2023-05-02 19:24:32 -0500

    Add the functionality for the add card of the form

--- src/app/save.js
+++ src/app/save.js
@@ -1,19 +1,29 @@
 const $main = document.querySelector(".recipes");
-const $title = document.getElementById("title__input");
-const $description = document.getElementById("description__input");
+const $titleInput = document.getElementById("title__input");
+const $descriptionInput = document.getElementById("description__input");
 const [$ingredients, $steps] = document.querySelectorAll(".register__list");
 const $save = document.querySelector(".register__save");
-const $frag_card = document.createDocumentFragment();
-const $card = `
+const $data = localStorage.getItem("data")
+  ? JSON.parse(localStorage.getItem("data"))
+  : [];
+function createRecipeCard(title, description, ingredients, steps) {
+  const $card = `
       <article class="recipes__cards">
         <div class="recipes__wrapper">
-          <h2 class="recipes__title"></h2>
+          <h2 class="recipes__title"> ${title}</h2>
           <p class="recipes__description">
+            ${description}
           </p>
         </div>
         <ol class="recipes__list recipes__list--ingredients">
+              ${ingredients
+                .map(item => `<li class="recipes__item">${item}</li>`)
+                .join("")}
         </ol>
         <ol class="recipes__list recipes__list--stpes">
+              ${steps
+                .map(item => `<li class="recipes__item">${item}</li>`)
+                .join("")}
         </ol>
         <div class="recipes__buttons">
           <button class="button button--settings">Editar</button>
@@ -21,14 +31,19 @@ const $card = `
         </div>
       </article>
   `;
-$save.addEventListener("click", () => {
-  $main.insertAdjacentHTML("beforeend", $card);
-  const $new_card = $main.lastElementChild;
-  const $list_in = $new_card.querySelector(".recipes__list--ingredients");
-  Array.from($ingredients.children).forEach(label => {
-    const $item = document.createElement("li");
-    $item.textContent = label.firstElementChild.value;
-    $frag_card.appendChild($item);
-  });
-  $list_in.appendChild($frag_card);
-});
+  return $card;
+}
+function addRecipeCard() {
+  const title = $titleInput.value;
+  const descriptin = $descriptionInput.value;
+  const ingredients = Array.from($ingredients.children).map(
+    label => label.firstElementChild.value
+  );
+  const steps = Array.from($steps.children).map(
+    label => label.firstElementChild.value
+  );
+  const $new_card = createRecipeCard(title, descriptin, ingredients, steps);
+  $main.insertAdjacentHTML("beforeend", $new_card);
+}
+function saveStorage() {}
+$save.addEventListener("click", () => addRecipeCard());
